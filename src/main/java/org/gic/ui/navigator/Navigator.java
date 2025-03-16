a2855df84af0552f7fe7fda724483e743ce9c691
Kyaw Soe <kyawsh@m1.com.sg>
2025-03-16 19:49:54 +0800

    feat: finished exit page

--- src/main/java/org/gic/ui/navigator/Navigator.java
+++ src/main/java/org/gic/ui/navigator/Navigator.java
@@ -12,7 +12,7 @@ public class Navigator {
             case GO_TO_PRINTING_STATEMENT_PAGE -> PrintStatementPage.showPrintStatementPage();
             case GO_TO_DEFINE_INTEREST_RULES_PAGE -> DefineInterestRulePage.showDefineInterestRulePage();
             case GO_TO_TRANSACTION_INPUT_PAGE -> TransactionPage.showInputTransactionPage();
-            case QUIT -> System.exit(0);
+            case QUIT -> ExitPage.showExitPage();
             default -> {
                 System.out.println("I don't know this route but let's just go to menu first " + navigationRoute);
                 MenuPage.showMenuPage();
---
+++ src/main/java/org/gic/ui/pages/ExitPage.java
@@ -0,0 +1,10 @@
+package org.gic.ui.pages;
+
+public class ExitPage {
+    public static void showExitPage() {
+        System.out.println();
+        System.out.println("Thank you for banking with AwesomeGIC Bank.");
+        System.out.println("Have a nice day!");
+        System.exit(0);
+    }
+}
